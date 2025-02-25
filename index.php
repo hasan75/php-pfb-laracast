e39a922bf7eb20df493cb3ed2b3c35ceb11ecff1
hasan75 <hasanjab14@gmail.com>
2025-02-25 17:51:25 +0600

    refactor(route): made a separate route file to controll route

--- index.php
+++ index.php
@@ -2,27 +2,5 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/home.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-function routeToController($uri, $routes): void {
-    if(array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}
-
-function abort($code = 404): void {
-    http_response_code(404);
-    require "views/{$code}.view.php";
-    die();
-}
-
-routeToController($uri, $route);
+require "route.php";
 
