2fbedaab56a56bf1c27b95363eba0b67d9dcf4e1
hasan75 <hasanjab14@gmail.com>
2025-02-25 17:38:12 +0600

    feat(separation): banner separated
    - added 404 Not Found Page\n - improved routes\n - separated banners from the views

--- index.php
+++ index.php
@@ -4,17 +4,14 @@ require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-//dd($uri)
-//
-//$routes = [
-//    '/' => 'controllers/home.php',
-//    '/contact' => 'controllers/contact.php',
-//    '/about' => 'controllers/about.php',
-//];
-//
-//
-if ($uri === '/') {
-    require 'controllers/home.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
+$routes = [
+    '/' => 'controllers/home.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+if(array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    require 'views/404.view.php';
 }
---
+++ views/404.view.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900"> 404! </h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            <p class="text-2xl font-semibold">Page Not Found!</p>
+            <a class="text-underline text-blue-400" href="/">Return to Home.</a>
+        </div>
+    </main>
+<?php require('partials/footer.php') ?>
