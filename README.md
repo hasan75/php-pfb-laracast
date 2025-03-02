091bfea2c28f4f0dfd26166a32dffde4745e286d
hasan75 <hasanjab14@gmail.com>
2025-03-02 13:09:56 +0600

    feat(pdo): class object declarations

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# PHP for Beginner
+
+Following Laracast's Php for Beginner Course By Jeffry.
+
+## Topics Covered:
+- Php Variables
+- Conditions and Booleans
+- Arrays
+- Associative Array
+- Functions and Filter
+- Lambda Functions
+- Page Links
+- Php Partials
+- Super Globals
+- Routes
+- MySQL and SQL
+- Php Data Object
+
+
--- index.php
+++ index.php
@@ -2,5 +2,19 @@
 
 require 'functions.php';
 
-require "route.php";
+//require "route.php";
 
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe() {
+        echo $this->name . ' is breathing';
+    }
+}
+
+$person = new Person();
+$person->name = 'Hasan Ahmed';
+$person->age = 26;
+
+$person->breathe();
