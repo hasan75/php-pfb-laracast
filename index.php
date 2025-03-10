cc80f137536a7d0f793f0dcfebb550be45079b88
hasan75 <hasanjab14@gmail.com>
2025-03-10 15:47:17 +0600

    feat(database-class): class created for database and having instance

--- index.php
+++ index.php
@@ -5,15 +5,23 @@ require 'functions.php';
 //require "route.php";
 
 
-// connect to our MySQL database
-$dsn = 'mysql:host=mysql;port=3306;dbname=demo;charset=utf8mb4';
+// connect to our MySQL database and execute a query
 
-$pdo = new PDO($dsn, 'root', 'root');
+//let's create a class
 
-$statement = $pdo->prepare("SELECT * FROM Posts");
-$statement->execute();
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+class Database {
+    public function query() {
+        $dsn = 'mysql:host=mysql;port=3306;dbname=demo;charset=utf8mb4';
 
+        $pdo = new PDO($dsn, 'root', 'root');
+
+        $statement = $pdo->prepare("SELECT * FROM Posts");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$posts = (new Database())->query();
 foreach ($posts as $post) {
     echo '<li>'. $post['title'] . ' - by ' . $post['author'] .'</li>';
 }
