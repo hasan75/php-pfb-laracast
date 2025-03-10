2229ccb673a7c227e6007cd2da64dae42b1ce432
hasan75 <hasanjab14@gmail.com>
2025-03-10 14:42:38 +0600

    feat(class-object): class and object decration in php and dockerfile changed

--- index.php
+++ index.php
@@ -4,17 +4,24 @@ require 'functions.php';
 
 //require "route.php";
 
+
+// class objects in php
+
 class Person {
     public $name;
     public $age;
 
-    public function breathe() {
-        echo $this->name . ' is breathing';
+    public function greets() {
+        echo "Hello, ". $this->name. "!";
     }
 }
 
 $person = new Person();
-$person->name = 'Hasan Ahmed';
-$person->age = 26;
+$person->name = 'Hasan';
+$person->age = 20;
 
-$person->breathe();
+//dd($person);
+//dd($person->name);
+$person->greets();
+
+
